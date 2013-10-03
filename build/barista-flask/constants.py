73240edf7d89ef126e953368beffdda888d5ab72
Sinan Midillili <sinan@hipo.biz>
2013-10-03 16:55:04 +0300

    working flask but getting 404

--- build/barista-flask/constants.py
+++ build/barista-flask/constants.py
@@ -1,3 +1,5 @@
 import os
 
-FLASKR_SETTINGS = 'settings.py'
+
+ROOT_URL = os.path.join(os.path.realpath(os.path.dirname('__file__')),'')
+# SETTINGS_URL = os.path.join(os.path.realpath(os.path.dirname('__file__')), '') + 'settings.py'
---
+++ build/barista-flask/settings.py
@@ -0,0 +1,6 @@
+class Config(object):
+    DATABASE = '/tmp/flaskr.db'
+    DEBUG = True
+    SECRET_KEY = 'dsagsdfadf234123'
+    USERNAME = 'admin'
+    PASSWORD = 'default'
