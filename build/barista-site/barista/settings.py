44e441b19718291a529557a4af0d1a51b7857ca4
Sinan Midillili <sinan@hipo.biz>
2013-10-25 14:06:49 +0300

    added bootstrap 3.0.0 and working nginx, things should be in order

--- build/barista-site/barista/settings.py
+++ build/barista-site/barista/settings.py
@@ -5,22 +5,10 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Sinan Midillili', 'sinan@hipo.biz'),
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -30,7 +18,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -51,18 +39,19 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(os.path.dirname( __file__ ), 'media/files/' )
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/static/files/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+# STATIC_ROOT = os.path.join(os.path.realpath(os.path.dirname( __file__ )), 'media/' )
+STATIC_ROOT = os.path.join( os.path.dirname( __file__ ), 'media/' )
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -73,6 +62,11 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    ("suit/", os.path.join(os.path.realpath(os.path.dirname(__file__)), 'media/suit/')),
+    ("static/css/", os.path.join(os.path.realpath(os.path.dirname(__file__)), 'media/css/')),
+    ("static/images/", os.path.join(os.path.realpath(os.path.dirname(__file__)), 'media/images/')),
+    ("static/js/", os.path.join(os.path.realpath(os.path.dirname(__file__)), 'media/js/')),
+    ("static/markitup/", os.path.join(os.path.realpath(os.path.dirname(__file__)), 'media/markitup/')),
 )
 
 # List of finder classes that know how to find static files in
@@ -80,7 +74,8 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+   # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
+
 )
 
 # Make this unique, and don't share it with anybody.
@@ -102,6 +97,17 @@ MIDDLEWARE_CLASSES = (
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+#
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+)
 
 ROOT_URLCONF = 'barista.urls'
 
@@ -113,23 +119,32 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(os.path.realpath(os.path.dirname(__file__)), 'templates/'),
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
+    # 'django.template.loaders.filesystem.Loader',
+    # 'django.template.loaders.app_directories.Loader',
 
 )
 
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
+   'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
+
+    'south',
+    # 'cuddlybuddly.thumbnail',
+    # 'cuddlybuddly.storage.s3',
+
+    'django_extensions',
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
+
     'django.contrib.admindocs',
+
+    # APPS GO HERE
+
 )
 
 # A sample logging configuration. The only tangible logging
@@ -160,3 +175,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from settings_local import *
+except ImportError:
+    pass
--- build/barista-site/barista/views.py
+++ build/barista-site/barista/views.py
@@ -1,11 +1,16 @@
 import os
 from django.views.generic import TemplateView
+from django.template import RequestContext
+from barista import settings
 
 
 class BaristaHomeView(TemplateView):
     template_name = "base.html"
-
+    # template_name = "examples/grid/index.html"
     def get(self, request, *args, **kwargs):
-        print os.path.join(os.path.realpath(os.path.dirname(__file__)))
+        print settings.STATIC_ROOT
         context = self.get_context_data(**kwargs)
+        # return render_to_response('example.html',{},context_instance=RequestContext(request))
+
+
         return self.render_to_response(context)
