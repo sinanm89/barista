a9bf551d1cd6d7f7159c5c4a3eb4a83dc02e7133
sinanm89 <sinanm89@gmail.com>
2014-05-29 11:42:15 -0400

    production commit

---
+++ conf/gunicorn.py
@@ -0,0 +1,6 @@
+command = '/home/snn/Projects/barista/bin/gunicorn'
+pythonpath = '/home/snn/Projects/barista'
+bind = '127.0.0.1:8000'
+workers = 3
+user = 'nobody'
+
---
+++ manage.py
@@ -0,0 +1,1 @@
+build/barista-site/manage.py
