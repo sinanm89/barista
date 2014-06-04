c2952080f326f86329c1420122be229a65b3b15f
sinanm89 <sinanm89@gmail.com>
2014-06-03 21:22:29 -0400

    working gunicorn configuration

--- build/barista-site/barista/settings.py
+++ build/barista-site/barista/settings.py
@@ -138,6 +138,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.gis',
 
+    'gunicorn',
     'south',
     'suit',
     'barista.restaurants',
--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,6 +1,5 @@
 command = '/home/snn/Projects/barista/bin/gunicorn'
-pythonpath = '/home/snn/Projects/barista'
+pythonpath = '/home/snn/Projects/barista/src/barista/build/barista-site'
 bind = '127.0.0.1:8000'
 workers = 3
 user = 'nobody'
-
