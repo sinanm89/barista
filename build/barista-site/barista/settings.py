6821b93209c2cec9e7ccf911aa9f811c0db0b389
sinanm89 <sinanm89@gmail.com>
2014-06-06 03:45:52 +0300

    suit config added

--- build/barista-site/barista/settings.py
+++ build/barista-site/barista/settings.py
@@ -150,6 +150,12 @@ INSTALLED_APPS = (
 
 )
 
+SUIT_CONFIG = {
+    'ADMIN_NAME': 'Barista',
+    'SHOW_REQUIRED_ASTERISK': True
+
+}
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
